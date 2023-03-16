efb2efb4293173d4568f046a459cf36ba418dc59
Nathan Hering <NathanH@gointerject.com>
2023-03-16 10:06:21 -0400

    ReturnedData documentation

--- interject-dotnet-api/Models/ReturnedData.cs
+++ interject-dotnet-api/Models/ReturnedData.cs
@@ -2,10 +2,24 @@ namespace Interject.Models
 {
     public class ReturnedData
     {
+        /// <summary>
+        /// Serialized instance of <see cref="InterjectTable"/>.
+        /// </summary>
         public object Data { get; set; } = new InterjectTable();
-        public int DataFormat { get; set; } = 2; // Hard code for reverse compatibility (DataFormat.JsonTableWithSchema)
-        public int SchemaFormat { get; set; } = 1; // Hard code for reverse compatibility (SchemaFormat.Interject_Object)
-        public object Schema { get; set; } = new();
+
+        /// <summary>
+        /// Hard coded for reverse compatibility (DataFormat.JsonTableWithSchema)
+        /// </summary>
+        /// <value>2</value>
+        public int DataFormat { get; set; } = 2;
+
+        /// <summary>
+        /// Hard code for reverse compatibility (SchemaFormat.Interject_Object)
+        /// </summary>
+        /// <value>1</value>
+        public int SchemaFormat { get; set; } = 1;
+
+        public object Schema { get; set; }
 
         public ReturnedData() { }
 
