12dba5ef86be24dec0b17d8a0ec1626a69fbe404
NathanHering <48037540+NathanHering@users.noreply.github.com>
2024-07-05 14:09:45 -0400

    Develop (#29)
    * Passthrough connectionstring (#16)
    
    * update date in license
    
    * concate passthrough connectionstring name with client id
    
    * Passthrough connectionstring (#18)
    
    * update date in license
    
    * concate passthrough connectionstring name with client id
    
    * change bad response to string in EnforceClientIdSecurity
    
    * resolve merge conflicts
    
    * Update to dotnet 8 (#21)
    
    * resolve warning
    
    * resolve a couple warnings and update using statements
    
    * update to net8
    
    * update launch.json with net8 folder ref
    
    * update param parser (#22)
    
    🐞 The param parser in the UserContext was only pulling the first role out instead of all the roles as a comma separated list.
    
    * Fix bug in validation key (#20)
    
    undefined
    
    * Fix bug in api (#24)
    
    * Develop (#19)
    
    * Passthrough connectionstring (#16)
    
    * update date in license
    
    * concate passthrough connectionstring name with client id
    
    * Passthrough connectionstring (#18)
    
    * update date in license
    
    * concate passthrough connectionstring name with client id
    
    * change bad response to string in EnforceClientIdSecurity
    
    * resolve merge conflicts
    
    * Fix bug in validation key
    
    * remove use of UseClientIdAsConnectionName setting
    
    * make lookup consistent
    
    ---------
    
    Co-authored-by: lyubar <lyubar@gmail.com>
    
    * Add documentation to publish to azure (#25)
    
    * Develop (#19)
    
    * Passthrough connectionstring (#16)
    
    * update date in license
    
    * concate passthrough connectionstring name with client id
    
    * Passthrough connectionstring (#18)
    
    * update date in license
    
    * concate passthrough connectionstring name with client id
    
    * change bad response to string in EnforceClientIdSecurity
    
    * resolve merge conflicts
    
    * Add documentation to publish to azure
    
    ---------
    
    Co-authored-by: lyubar <lyubar@gmail.com>
    
    * update versions
    
    * Added rowdef and rowdefname endpoints
    
    * updated docs for multi rowdef cols
    
    * fixed brackets and updated example.xlsx
    
    * Updated Readme
    
    * Ijp 3507 update (#27)
    
    * Added jDropdown and Parameters endpoint
    
    * Added jdropdown and reportlookup to docs and examples
    
    * sync docs with other apis
    
    * update version
    
    ---------
    
    Co-authored-by: Eugene Lyubar <lyubar@gmail.com>
    Co-authored-by: Samuel Riesterer <113140692+samuel-riesterer-interject@users.noreply.github.com>

--- interject.data.api/interject-dotnet-api/ApplicationOptions.cs
+++ interject.data.api/interject-dotnet-api/ApplicationOptions.cs
@@ -5,8 +5,8 @@ namespace Interject.DataApi
         public const string Application = "Application";
 
         public string Name { get; set; } = "Interject Data Api";
-        public string Version { get; set; } = "1.1.2";
-        public string Framework { get; set; } = "net7.0";
+        public string Version { get; set; } = "1.1.7";
+        public string Framework { get; set; } = "net8.0";
         public bool UseClientIdAsConnectionName { get; set; } = false;
 
         public ApplicationOptions() { }
